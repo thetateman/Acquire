bc940a31af4fac0cdfac0619454db912b361b469
Tate Smith <tdsmith2018@gmail.com>
2022-06-07 16:21:48 -0500

    working on dark-mode on game page

--- client/src/common/dark_mode_toggle.js
+++ client/src/common/dark_mode_toggle.js
@@ -1,8 +1,23 @@
+/**
+ * Add this script to any page we need the dark mode toggle.
+ * Assumes document body has class 'dark-mode'.
+ */
 (() => {
+    // Add toggle to page
+    const toggleHTML = 
+    `<label class="switch">
+        <input id="color-scheme-toggle" type="checkbox">
+        <span class="slider round"></span>
+        <span class="color-label" id="light-label">Light</span>
+        <span class="color-label" id="dark-label">Dark</span>
+    </label>`;
+    document.body.insertAdjacentHTML('beforeend', toggleHTML);
+
     if(!('dark_mode' in localStorage)){
         localStorage.dark_mode = 'true';
     }
     if(localStorage.dark_mode === 'false'){
+        // Turn off dark mode, set toggle
         document.body.classList.toggle('dark-mode');
         document.querySelector('#color-scheme-toggle').checked = true;
     }
