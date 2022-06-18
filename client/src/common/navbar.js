07b515758e6a17a4e453f103f16c7c3487ae6e4d
thetateman <tdsmith2018@gmail.com>
2022-06-17 20:10:58 -0500

    navbar created in js, shows page location

--- client/src/common/navbar.js
+++ client/src/common/navbar.js
@@ -16,6 +16,23 @@ const logout = () => {
 };
 
 (() => {
+    const navbarHTML = `
+        <div class="topnav">
+            <a href="/lobby">Lobby</a>
+            <a href="#news">Game Stats</a>
+            <a href="/about">About</a>
+            <div class="topnav-right">
+                <a id="navbar-username">${localStorage.username}</a>
+                <a href="#" id="logout-button">Logout</a>
+            </div>
+        </div>`;
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+
+    // set active class on link representing current page
+    const page = window.location.href.split("/")[window.location.href.split("/").length - 1];
+    document.querySelector(`.topnav a[href='/${page}']`).classList.toggle('active');
+
+
     document
     .querySelector('#logout-button')
     .addEventListener('click', logout);
